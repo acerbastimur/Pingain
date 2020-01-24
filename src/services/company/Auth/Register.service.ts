8d5e34ae38d15619c75ae123358c84180f960a1f
acerbastimur <umutacerbastimur@gmail.com>
2020-01-24 03:09:47 +0300

    get statistics on register

--- src/services/company/Auth/Register.service.ts
+++ src/services/company/Auth/Register.service.ts
@@ -1,5 +1,6 @@
 import auth, {FirebaseAuthTypes} from '@react-native-firebase/auth';
 import firestore, {FirebaseFirestoreTypes} from '@react-native-firebase/firestore';
+import {CompanyStatistics} from '../../../schemes/Company';
 
 export default class RegisterService {
   static registerCompanyAuth(
@@ -28,12 +29,19 @@ export default class RegisterService {
     return new Promise((resolve, reject) => {
       const userUid = auth().currentUser.uid;
 
+      const statistics: CompanyStatistics = {
+        monthlyPinGiven: 0,
+        monthlyPrizeGiven: 0,
+        totalPinGiven: 0,
+        totalPrizeGiven: 0,
+      };
       firestore()
         .collection('companies')
         .doc(userUid)
         .set({
           email,
           registerDate,
+          statistics,
         })
         .then(result => {
           console.log('ok');
