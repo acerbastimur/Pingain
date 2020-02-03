440bcfcbd6616cb0c00d0bbc3d7ac19ce150c5e7
acerbastimur <umutacerbastimur@gmail.com>
2020-02-03 22:54:27 +0300

    touchable opacity customize

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
 import * as React from 'react';
-import {View, Text, Button, SafeAreaView, StyleSheet} from 'react-native';
+import {View, Text, Button, SafeAreaView, StyleSheet, TouchableOpacity} from 'react-native';
 import 'react-native-gesture-handler';
+import customize from 'react-native-default-props';
 import Navigation from './src/navigations/Navigation';
+
+customize(TouchableOpacity, {
+  activeOpacity: 0.8,
+});
 
 export default class App extends React.Component {
   constructor(props: any) {
