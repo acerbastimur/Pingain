2012efcf5d55c9c7764b8705a019c5b105c02de1
acerbastimur <umutacerbastimur@gmail.com>
2020-02-24 00:36:17 +0300

    multidex error

--- android/app/src/main/java/com/pingain/MainApplication.java
+++ android/app/src/main/java/com/pingain/MainApplication.java
@@ -2,6 +2,9 @@ package com.pingain;
 
 import android.app.Application;
 import android.content.Context;
+
+import androidx.multidex.MultiDex;
+
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
@@ -45,4 +48,11 @@ private final ReactNativeHost mReactNativeHost =
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
   }
+
+
+@Override 
+protected void attachBaseContext(Context base) {
+    super.attachBaseContext(base);
+    MultiDex.install(this);
+} 
 }
