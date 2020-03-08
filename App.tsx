5bfe8d6e130d2f31d12b8c24153c4aa971eff441
acerbastimur <umutacerbastimur@gmail.com>
2020-03-08 16:57:53 +0300

    push notification is integrated

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import customize from 'react-native-default-props';
 import CodePush, { CodePushOptions } from 'react-native-code-push';
 import SplashScreen from 'react-native-splash-screen';
 import crashlytics from '@react-native-firebase/crashlytics';
+import messaging from '@react-native-firebase/messaging';
 import Navigation from './src/navigations/Navigation';
 
 customize(TouchableOpacity, {
@@ -13,6 +14,7 @@ customize(TouchableOpacity, {
 class App extends React.Component {
   componentDidMount() {
     crashlytics().setCrashlyticsCollectionEnabled(true);
+    messaging().registerForRemoteNotifications();
 
     CodePush.sync({
       installMode: CodePush.InstallMode.ON_NEXT_RESTART,
@@ -23,6 +25,9 @@ class App extends React.Component {
       .catch(() => {
         SplashScreen.hide();
       });
+    messaging().setBackgroundMessageHandler(async remoteMessage => {
+      console.log('Message handled in the background!', remoteMessage);
+    });
   }
 
   public render() {
